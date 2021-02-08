aeb539ef48ac3b3ccc8320d66d6504d203c4a15e
Kony <fzf55747@kwansei.ac.jp>
2021-02-08 14:13:45 +0900

    Plus, Minus and Clear

--- countapp/ViewController.swift
+++ countapp/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var lavel: UILabel!
+    var number:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus(){
+        number = number + 1
+        lavel.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        lavel.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        lavel.text = String(number)
     }
 
 
